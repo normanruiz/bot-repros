71d884f99e5ac50f1ef99429e380d20b57b8e685
Norman Ruiz <norman.ruiz@outlook.com.ar>
2022-10-09 22:32:06 -0300

    Agrego modulo Logger para manejo de logs.

--- bot.rb
+++ bot.rb
@@ -1,24 +1,48 @@
+
+require './files_bot/logger.rb'
+
 class Bot
+	include Logger
+
 	attr_accessor :estado
+
 	def run
 		@estado = 0
-		system('clear') 
+		begin
+		system('clear')
+		verificar_log()
 		puts("")
 		puts(" " + "=" * 128)
-		mensaje = "Iniciando Repro's Bot..." 
+		mensaje = "Iniciando Repro's Bot..."
 		puts("  " + mensaje)
+		escribir_log(mensaje)
 		mensaje = " #{'~' * 128}"
-		puts(mensaje)
+		escribir_log(mensaje, false)
+		puts(" " + "~" * 128)
 		puts("")
 
+
+
 		mensaje = " #{'~' * 128}"
-		puts(mensaje)
+		escribir_log(mensaje, false)
+		puts(" " + "~" * 128)
 		mensaje = "Finalizando Repro's Bot..."
 		puts("  " + mensaje)
+		escribir_log(mensaje)
 		mensaje = " #{'=' * 128}"
 		puts(mensaje)
+		escribir_log(mensaje, false)
 		puts("")
+
+		rescue Excetion => excepcion
+			@estado = 1
+			puts("  #{Datetime.now.to_str} - ERROR - Ejecicion principal - #{excepcion.message}")
+		ensure
+			return @estado
+		end
+
 	end
+
 end
 
 bot = Bot.new
---
+++ files_bot/logger.rb
@@ -0,0 +1,57 @@
+
+require 'date'
+
+module Logger
+	def verificar_log
+		estado = true
+		fecha = Date.today
+		carpeta_log = File.join('.', 'files_log')
+		archivo_log = "log-#{fecha}.txt"
+		begin
+			if File.directory?(carpeta_log)
+				unless File.file?(File.join(carpeta_log, archivo_log)) then
+					crear_archivo(carpeta_log, archivo_log)
+				end
+			else
+				Dir.mkdir(carpeta_log)
+				crear_archivo(carpeta_log, archivo_log)
+			end
+		rescue Exception => excepcion
+			estado = false
+			puts("  #{DateTime.now} - ERROR - Escribiendo log - #{excepcion.message}")
+		ensure
+			return estado
+		end
+	end
+
+	def escribir_log(linea, tiempo = true)
+		estado = true
+		fecha = Date.today
+		hora = DateTime.now.strftime('%T')
+		carpeta_log = File.join('.', 'files_log')
+		archivo_log = "log-#{fecha}.txt"
+		begin
+			file_log = File.open(File.join(carpeta_log, archivo_log), 'a')
+			if tiempo
+				file_log.write("  #{hora} - #{linea}\n")
+			else
+				file_log.write("#{linea}\n")
+			end
+			file_log.close()
+		rescue Exception => excepcion
+			estado = false
+			puts("  #{DateTime.now} - ERROR - Escribiendo log - #{excepcion.message}")
+		ensure
+			return estado
+		end
+	end
+
+	private
+	def crear_archivo(carpeta_log, archivo_log)
+		file_log = File.new(File.join(carpeta_log, archivo_log), 'w')
+		file_log.write(" " + "=" * 128 + "\n")
+		file_log.write("  #{DateTime.now} - Archivo de log generado\n")
+		file_log.write(" " + "~" * 128 + "\n")
+		file_log.close()
+	end
+end
